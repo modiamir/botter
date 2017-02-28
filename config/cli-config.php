ad433eb0829d031f3d458d9085525cdbc3ce5a58
modiamir <amir.modarresi.13@gmail.com>
2017-02-28 18:20:06 +0330

    fixed cli-config.php problem

--- config/cli-config.php
+++ config/cli-config.php
@@ -6,8 +6,6 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
 
 $sc = include __DIR__ . '/../Botter/container.php';
-$loader = new YamlFileLoader($sc, new FileLocator(__DIR__));
-$loader->load('services.yml');
 
 return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($sc->get('entity_manager'));
 
